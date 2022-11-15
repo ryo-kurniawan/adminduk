c7f4adcbf8f3df83aa02d34e34fad4ac5c996a7e
Ryo Kurniawan <pakayaaryo@gmail.com>
2022-11-15 22:09:10 +0700

    Merubah posisi Tanggal pernikahan

--- application/modules/kua/views/v_kua.php
+++ application/modules/kua/views/v_kua.php
@@ -8,7 +8,7 @@
                             <?= $subJudul ?>
                         </h6>
                         <?php if ($dataLogin['role'] == 'operator_kua') : ?>
-                        <a href="<?= base_url('kua/create') ?>" class="btn btn-light">Tambah Data</a>
+                            <a href="<?= base_url('kua/create') ?>" class="btn btn-light">Tambah Data</a>
                         <?php endif; ?>
                     </div>
                 </div>
@@ -22,117 +22,102 @@
                                     <th>Mempelai Wanita</th>
                                     <th>Penghulu</th>
                                     <th>Saksi</th>
-                                    <th>Status</th>
                                     <th>
                                         Tanggal <br> Pernikahan
                                     </th>
+                                    <th>Status</th>
+
                                     <th>Aksi</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <?php $i=1; foreach ($dataKua as $key) : ?>
-                                <tr>
-                                    <td><?= $i++ ?></td>
-                                    <td>
-                                        <div class="text-dark"><?= $key['nama_pria'] ?></div>
-                                        <small><?= $key['nik_pria'] ?></small>
-                                    </td>
-                                    <td>
-                                        <div class="text-dark"><?= $key['nama_wanita'] ?></div>
-                                        <small><?= $key['nik_wanita'] ?></small>
-                                    </td>
-                                    <td>
-                                        <div class="text-dark"><?= $key['nama_penghulu'] ?></div>
-                                        <small><?= $key['nik_penghulu'] ?></small>
-                                    </td>
-                                    <td>
-                                        <div class="text-dark"><?= $key['nama_saksi_1'] ?></div>
-                                        <small><?= $key['nik_saksi_1'] ?></small>
-                                        <div class="text-dark"><?= $key['nama_saksi_2'] ?></div>
-                                        <small><?= $key['nik_saksi_2'] ?></small>
-                                    </td>
-                                    <td>
-                                        <span class="border
+                                <?php $i = 1;
+                                foreach ($dataKua as $key) : ?>
+                                    <tr>
+                                        <td><?= $i++ ?></td>
+                                        <td>
+                                            <div class="text-dark"><?= $key['nama_pria'] ?></div>
+                                            <small><?= $key['nik_pria'] ?></small>
+                                        </td>
+                                        <td>
+                                            <div class="text-dark"><?= $key['nama_wanita'] ?></div>
+                                            <small><?= $key['nik_wanita'] ?></small>
+                                        </td>
+                                        <td>
+                                            <div class="text-dark"><?= $key['nama_penghulu'] ?></div>
+                                            <small><?= $key['nik_penghulu'] ?></small>
+                                        </td>
+                                        <td>
+                                            <div class="text-dark"><?= $key['nama_saksi_1'] ?></div>
+                                            <small><?= $key['nik_saksi_1'] ?></small>
+                                            <div class="text-dark"><?= $key['nama_saksi_2'] ?></div>
+                                            <small><?= $key['nik_saksi_2'] ?></small>
+                                        </td>
+                                        <td><?= $key['tgl_nikah'] ? date_indo($key['tgl_nikah']) : '-' ?></td>
+                                        <td>
+                                            <span class="border
                                             <?php
-                                                switch ($key['status']) {
-                                                    case 'proses':
-                                                        echo 'border-warning text-warning';
-                                                        break;
+                                            switch ($key['status']) {
+                                                case 'proses':
+                                                    echo 'border-warning text-warning';
+                                                    break;
 
-                                                    case 'revisi':
-                                                        echo 'border-warning text-warning';
-                                                        break;
+                                                case 'revisi':
+                                                    echo 'border-warning text-warning';
+                                                    break;
 
-                                                    case 'tervalidasi':
-                                                        echo 'border-info text-info';
-                                                        break;
+                                                case 'tervalidasi':
+                                                    echo 'border-info text-info';
+                                                    break;
 
-                                                    case 'selesai':
-                                                        echo 'border-success text-success';
-                                                        break;
-                                                    
-                                                    default:
-                                                        echo 'border-dark text-dark';
-                                                        break;
-                                                }
+                                                case 'selesai':
+                                                    echo 'border-success text-success';
+                                                    break;
+
+                                                default:
+                                                    echo 'border-dark text-dark';
+                                                    break;
+                                            }
                                             ?> 
                                             rounded py-1 px-2 fs-7 text-uppercase"><?= $key['status'] ?>
-                                        </span>
-                                    </td>
-                                    <td><?= $key['tgl_nikah'] ? date_indo($key['tgl_nikah']) : '-' ?></td>
-                                    <td class="td-actions text-right">
-                                        <?php if ($dataLogin['role'] == 'operator_kua') : ?>
-                                        <a href="<?= base_url().'kua/detail/'.$key['id'] ?>" type="button" class="btn btn-info p-2"
-                                            data-bs-toggle="tooltip" data-bs-placement="top"
-                                            data-bs-custom-class="custom-tooltip"
-                                            data-bs-title="Detail">
-                                            <i class="material-icons">info_outline</i>
-                                        </a>
-                                            <?php if ($key['status'] != 'tervalidasi') : ?>
-                                                <?php if ($key['status'] != 'selesai') : ?>
-                                                <a href="<?= base_url().'kua/edit/'.$key['id'] ?>" type="button" class="btn btn-success p-2"
-                                                    data-bs-toggle="tooltip" data-bs-placement="top"
-                                                    data-bs-custom-class="custom-tooltip"
-                                                    data-bs-title="Edit">
-                                                    <i class="material-icons">edit</i>
+                                            </span>
+                                        </td>
+
+                                        <td class="td-actions text-right">
+                                            <?php if ($dataLogin['role'] == 'operator_kua') : ?>
+                                                <a href="<?= base_url() . 'kua/detail/' . $key['id'] ?>" type="button" class="btn btn-info p-2" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" data-bs-title="Detail">
+                                                    <i class="material-icons">info_outline</i>
                                                 </a>
-                                                <button type="button" class="btn btn-danger p-2" onclick="return sweetConfirm(event, 'Anda akan menghapus file ini', '<?= base_url().'kua/delete/'.$key['id'] ?>')"
-                                                    data-bs-toggle="tooltip" data-bs-placement="top"
-                                                    data-bs-custom-class="custom-tooltip"
-                                                    data-bs-title="Hapus">
-                                                    <i class="material-icons">delete_forever</i>
-                                                </button>
+                                                <?php if ($key['status'] != 'tervalidasi') : ?>
+                                                    <?php if ($key['status'] != 'selesai') : ?>
+                                                        <a href="<?= base_url() . 'kua/edit/' . $key['id'] ?>" type="button" class="btn btn-success p-2" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" data-bs-title="Edit">
+                                                            <i class="material-icons">edit</i>
+                                                        </a>
+                                                        <button type="button" class="btn btn-danger p-2" onclick="return sweetConfirm(event, 'Anda akan menghapus file ini', '<?= base_url() . 'kua/delete/' . $key['id'] ?>')" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" data-bs-title="Hapus">
+                                                            <i class="material-icons">delete_forever</i>
+                                                        </button>
+                                                    <?php endif; ?>
+                                                <?php else : ?>
+                                                    <button type="button" class="btn btn-success p-2" onclick="return sweetConfirm(event, 'Realisasi Data?', '<?= base_url() . 'kua/realisasi/' . $key['id'] ?>')" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" data-bs-title="Realisasi">
+                                                        <i class="material-icons">done_all</i>
+                                                    </button>
                                                 <?php endif; ?>
-                                            <?php else : ?>
-                                            <button type="button" class="btn btn-success p-2" onclick="return sweetConfirm(event, 'Realisasi Data?', '<?= base_url().'kua/realisasi/'.$key['id'] ?>')"
-                                                data-bs-toggle="tooltip" data-bs-placement="top"
-                                                data-bs-custom-class="custom-tooltip"
-                                                data-bs-title="Realisasi">
-                                                <i class="material-icons">done_all</i>
-                                            </button>
                                             <?php endif; ?>
-                                        <?php endif; ?>
-                                        <?php if ($dataLogin['role'] == 'operator_capil') : ?>
-                                            <?php if ($key['status'] == 'proses') : ?>
-                                            <a href="<?= base_url().'kua/validasi/'.$key['id'] ?>" type="button" class="btn btn-success p-2"
-                                                data-bs-toggle="tooltip" data-bs-placement="top"
-                                                data-bs-custom-class="custom-tooltip"
-                                                data-bs-title="Validasi">
-                                                <i class="material-icons">check</i>
-                                                Validasi
-                                            </a>
-                                            <?php else : ?>
-                                            <a href="<?= base_url().'kua/detail/'.$key['id'] ?>" type="button" class="btn btn-info p-2"
-                                                data-bs-toggle="tooltip" data-bs-placement="top"
-                                                data-bs-custom-class="custom-tooltip"
-                                                data-bs-title="Detail">
-                                                <i class="material-icons">info_outline</i>
-                                                Detail
-                                            </a>
+                                            <?php if ($dataLogin['role'] == 'operator_capil') : ?>
+                                                <?php if ($key['status'] == 'proses') : ?>
+                                                    <a href="<?= base_url() . 'kua/validasi/' . $key['id'] ?>" type="button" class="btn btn-success p-2" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" data-bs-title="Validasi">
+                                                        <i class="material-icons">check</i>
+                                                        Validasi
+                                                    </a>
+                                                <?php else : ?>
+                                                    <a href="<?= base_url() . 'kua/detail/' . $key['id'] ?>" type="button" class="btn btn-info p-2" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" data-bs-title="Detail">
+                                                        <i class="material-icons">info_outline</i>
+                                                        Detail
+                                                    </a>
+                                                <?php endif; ?>
                                             <?php endif; ?>
-                                        <?php endif; ?>
-                                    </td>
-                                </tr>
+                                        </td>
+                                    </tr>
                                 <?php endforeach; ?>
                             </tbody>
                         </table>
