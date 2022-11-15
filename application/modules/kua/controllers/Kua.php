769e17a4b7c5b045b443a9084435e0b09f143384
Ryo Kurniawan <pakayaaryo@gmail.com>
2022-11-15 22:50:56 +0700

    Update Final Module KUA

--- application/modules/kua/controllers/Kua.php
+++ application/modules/kua/controllers/Kua.php
@@ -10,30 +10,30 @@ class Kua extends MX_Controller
 		}
 		$this->load->model('M_kua');
 		$this->load->library('form_validation');
-        $this->load->model('template/Model_template', 'mt');
+		$this->load->model('template/Model_template', 'mt');
 		$this->load->helper('tgl_indo');
 	}
 
 	public function getUserLogin()
-    {
-        if ($this->session->userdata('nik')) {
-            $getUser = $this->mt->getWhere('t_user', ['nik' => $this->session->userdata('nik')]);
-            if ($getUser->num_rows() > 0) {
-                $row = $getUser->row_array();
-                $dataLogin = [
-                    'nik' => $row['nik'],
-                    'nama' => $row['name'],
-                    'email' => $row['email'],
-                    'username' => $row['username'],
-                    'avatar' => $row['avatar'],
-                    'role' => $row['role'],
-                    'created_at' => $row['created_at'],
-                    'last_login' => $row['last_login']
-                ];
-                return $dataLogin;
-            }
-        }
-    }
+	{
+		if ($this->session->userdata('nik')) {
+			$getUser = $this->mt->getWhere('t_user', ['nik' => $this->session->userdata('nik')]);
+			if ($getUser->num_rows() > 0) {
+				$row = $getUser->row_array();
+				$dataLogin = [
+					'nik' => $row['nik'],
+					'nama' => $row['name'],
+					'email' => $row['email'],
+					'username' => $row['username'],
+					'avatar' => $row['avatar'],
+					'role' => $row['role'],
+					'created_at' => $row['created_at'],
+					'last_login' => $row['last_login']
+				];
+				return $dataLogin;
+			}
+		}
+	}
 
 	public function index()
 	{
@@ -73,12 +73,12 @@ class Kua extends MX_Controller
 	{
 		$data = $this->input->post();
 		$files = $_FILES;
-		if($this->M_kua->submit($data, $files)) {
+		if ($this->M_kua->submit($data, $files)) {
 			$this->session->set_flashdata('success', 'Berhasil Menambahkan Data!');
 			redirect('kua');
 		} else {
 			$this->session->set_flashdata('danger', 'Gagal Menambahkan Data!');
-			redirect('kua/create');	
+			redirect('kua/create');
 		}
 	}
 
@@ -103,12 +103,12 @@ class Kua extends MX_Controller
 	{
 		$data = $this->input->post();
 		$files = $_FILES;
-		if($this->M_kua->edit($data, $files)) {
+		if ($this->M_kua->edit($data, $files)) {
 			$this->session->set_flashdata('success', 'Berhasil Mengedit Data!');
-			redirect('kua/detail/'.$data['id']);
+			redirect('kua/detail/' . $data['id']);
 		} else {
 			$this->session->set_flashdata('danger', 'Gagal Mengedit Data!');
-			redirect('kua/edit/'.$data['id']);
+			redirect('kua/edit/' . $data['id']);
 		}
 	}
 
@@ -142,6 +142,7 @@ class Kua extends MX_Controller
 		$id = $this->uri->segment(3);
 		$result = $this->db->where('id', $id)->get('t_formulir_kua')->result()[0];
 		$r['data'] = $result;
+		$r['revisi'] = $this->db->where('form_kua_id', $id)->get('t_revisi_formulir_kua')->last_row();
 		$r['validasi'] = true;
 		$r['dataLogin'] = Modules::run('template/getUserLogin');
 		echo Modules::run('template/templateIsLogin', $r);
@@ -149,50 +150,50 @@ class Kua extends MX_Controller
 
 	public function validation()
 	{
-		if($this->getUserLogin()['role'] != 'operator_capil') {
+		if ($this->getUserLogin()['role'] != 'operator_capil') {
 			$this->session->set_flashdata('danger', 'Gagal!');
-			redirect('kua');	
+			redirect('kua');
 		}
 		$id = $this->uri->segment(3);
-		if($this->M_kua->validation($id)) {
+		if ($this->M_kua->validation($id)) {
 			$this->session->set_flashdata('success', 'Validasi data berhasil!');
-			redirect('kua/detail/'.$id);
+			redirect('kua/detail/' . $id);
 		} else {
 			$this->session->set_flashdata('danger', 'Validasi data gagal!');
-			redirect('kua/validasi/'.$id);
+			redirect('kua/validasi/' . $id);
 		}
 	}
 
 	public function revisi()
 	{
-		if($this->getUserLogin()['role'] != 'operator_capil') {
+		if ($this->getUserLogin()['role'] != 'operator_capil') {
 			$this->session->set_flashdata('danger', 'Gagal!');
-			redirect('kua');	
+			redirect('kua');
 		}
 		$id = $this->uri->segment(3);
 		$data = $this->input->post();
-		if($this->M_kua->revisi($id, $data)) {
+		if ($this->M_kua->revisi($id, $data)) {
 			$this->session->set_flashdata('success', 'Revisi data berhasil!');
-			redirect('kua/detail/'.$id);
+			redirect('kua/detail/' . $id);
 		} else {
 			$this->session->set_flashdata('danger', 'Revisi data gagal!');
-			redirect('kua/validasi/'.$id);
+			redirect('kua/validasi/' . $id);
 		}
 	}
 
 	public function realisasi()
 	{
-		if($this->getUserLogin()['role'] != 'operator_kua') {
+		if ($this->getUserLogin()['role'] != 'operator_kua') {
 			$this->session->set_flashdata('danger', 'Gagal!');
-			redirect('kua');	
+			redirect('kua');
 		}
 		$id = $this->uri->segment(3);
-		if($this->M_kua->realisasi($id)) {
+		if ($this->M_kua->realisasi($id)) {
 			$this->session->set_flashdata('success', 'Realisasi data berhasil!');
-			redirect('kua/detail/'.$id);
+			redirect('kua/detail/' . $id);
 		} else {
 			$this->session->set_flashdata('danger', 'Realisasi data gagal!');
-			redirect('kua/realisasi/'.$id);
+			redirect('kua/realisasi/' . $id);
 		}
 	}
 }
